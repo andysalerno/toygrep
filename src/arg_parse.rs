8efd89aaa60e5e63f69e82d2559e6f44c2f6a95c
andy <andysalerno@gmail.com>
2019-11-23 14:47:46 -0800

    Fix lint issues, and print once at the end instead of during iteration.

--- src/arg_parse.rs
+++ src/arg_parse.rs
@@ -1,29 +1,32 @@
 use peeking_take_while::PeekableExt;
 
 #[derive(Debug, Default)]
-pub struct UserInput {
-    pub search_pattern: String,
-    pub search_targets: Vec<String>,
+pub(crate) struct UserInput {
+    pub(crate) debug_enabled: bool,
 
-    pub recursive: bool,
-    pub whole_word: bool,
-    pub case_insensitive: bool,
+    pub(crate) search_pattern: String,
+    pub(crate) search_targets: Vec<String>,
+
+    pub(crate) recursive: bool,
+    pub(crate) whole_word: bool,
+    pub(crate) case_insensitive: bool,
 }
 
 /// Parses the given arguments, following this expected format:
 /// toygrep [OPTION]... PATTERN [FILE]...
-pub fn capture_input(args: impl Iterator<Item = String>) -> UserInput {
+pub(crate) fn capture_input(args: impl Iterator<Item = String>) -> UserInput {
     let mut user_input = UserInput::default();
 
     let mut args = args.skip(1).peekable();
 
     // Flags come first.
-    for arg in args.by_ref().peeking_take_while(|a| a.starts_with("-")) {
+    for arg in args.by_ref().peeking_take_while(|a| a.starts_with('-')) {
         // TODO: support combined flags, like '-iwr'
         match arg.as_str() {
             "-i" => user_input.case_insensitive = true,
             "-w" => user_input.whole_word = true,
             "-r" => user_input.recursive = true,
+            "-d" => user_input.debug_enabled = true,
             _ => {
                 panic!("Unknown flag: {}", arg);
             }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+//! Documentation in progress.
+
+#![forbid(unsafe_code, rust_2018_idioms)]
+#![deny(
+    missing_debug_implementations,
+    nonstandard_style,
+    trivial_casts,
+    trivial_numeric_casts
+)]
+#![warn(
+    missing_docs,
+    missing_doc_code_examples,
+    unreachable_pub,
+    future_incompatible
+)]
+
 mod arg_parse;
 
 use async_std::fs;
@@ -10,28 +26,32 @@ async fn main() -> IoResult<()> {
 
     let user_input = arg_parse::capture_input(args);
 
-    dbg!(&user_input);
+    if user_input.debug_enabled {
+        dbg!(&user_input);
+    }
 
-    let regex = Regex::new(&user_input.search_pattern).expect(&format!(
-        "Invalid search expression: {}",
-        &user_input.search_pattern
-    ));
+    let regex = Regex::new(&user_input.search_pattern)
+        .unwrap_or_else(|_| panic!("Invalid search expression: {}", &user_input.search_pattern));
 
-    search_file(&user_input.search_targets[0], &regex).await?;
+    let search_result = search_file(&user_input.search_targets[0], &regex).await?;
+
+    println!("{}", search_result);
 
     Ok(())
 }
 
-async fn search_file(file_path: &str, pattern: &Regex) -> IoResult<()> {
+async fn search_file(file_path: &str, pattern: &Regex) -> IoResult<String> {
     let content = fs::read_to_string(file_path).await?;
+    let mut result = String::new();
 
     let lines = content.lines();
 
     for line in lines {
         if pattern.is_match(line) {
-            println!("Found match: {}", line);
+            result.push_str(line);
+            result.push('\n');
         }
     }
 
-    Ok(())
+    Ok(result)
 }
