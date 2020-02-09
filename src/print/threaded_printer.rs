ffd4e187c0bc9198dcd87529693c779665e684f2
andy <andysalerno@gmail.com>
2020-02-09 11:17:18 -0800

    Re-add logging for time-to-first-print.

--- src/print/threaded_printer.rs
+++ src/print/threaded_printer.rs
@@ -47,13 +47,19 @@ impl<M: Matcher + 'static> Printer<M> {
 
         // At first, the instant represents 'spawn-to-first-print'.
         let spawn_to_print_instant = Instant::now();
-        // Bit of a hack -- not using `instant` the intended way here.
         let mut time_log = TimeLog::new(spawn_to_print_instant);
+        let mut has_logged = false;
 
         while let Ok(message) = self.receiver.recv() {
+            if !has_logged {
+                time_log.log_printer_spawn_to_print();
+                has_logged = true;
+            }
+
             self.printer.print(&mut stdout, message);
         }
 
+        time_log.log_print_duration();
         time_log
     }
 }
