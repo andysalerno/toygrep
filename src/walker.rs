88b72c60212b64a7380fa6342c962de65870b1d1
andy <andy@pop-os.localdomain>
2020-06-08 22:54:40 -0700

    begin attempt to implement a new walker

--- src/walker.rs
+++ src/walker.rs
@@ -2,27 +2,39 @@ use async_std::path::PathBuf;
 use core::future::Future;
 
 struct Walker<F>
-where F: Future<Output = ()> + Send + 'static
+where F: Fn() -> Future<Output = ()> + Clone + Send + 'static
 {
     path: PathBuf,
     action: F 
 }
 
 impl<F> Walker<F>
-where F: Future<Output = ()> + Send + 'static
+where F: Fn() -> Future<Output = ()> + Clone + Send + 'static
  {
-    fn new(path: PathBuf, action: F) -> Self {
+    fn new(path: PathBuf, action: Box<F>) -> Self {
         Walker { path, action }
     }
 
     async fn run(self) {
         async_std::task::spawn(self.action).await;
     }
+
+    async fn walk(self) {
+        use async_std::prelude::*;
+
+        let mut dir_entries = async_std::fs::read_dir(self.path).await.unwrap();
+        while let Some(entry) = dir_entries.next().await {
+            let entry_path = entry.unwrap().path();
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    // Pick up here:
+    // https://github.com/BurntSushi/ripgrep/blob/master/crates/ignore/src/walk.rs#L1193
 
     #[test]
     fn try_run_future() {
@@ -31,6 +43,7 @@ mod test {
                 println!("\n\n\nyo!!\n\n");
                 assert!(false, "yo!!!");
             });
+
             walker.run().await;
         });
     }
